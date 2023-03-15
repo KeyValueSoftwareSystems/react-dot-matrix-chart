0390cf27e232422a8eef238d540df1ff51405c3d
aarathyKeyvalue <aarathy@keyvalue.systems>
2023-03-15 12:01:28 +0530

    Responsive design - chart

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
+/* eslint-disable no-undef */
 module.exports = {
-    // Other configuration above...
+  // Other configuration above...
 
-    // Add the next three options if using TypeScript
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
-    preset: "ts-jest",
-    roots: ["<rootDir>/src"],
-    transform: {
-        "^.+\\.[t|j]sx?$": "ts-jest",
-        "^.+\\.svg?$": "<rootDir>/transform.js",
-        "^.+\\.scss?$": "<rootDir>/transform.js"
-    },
-    "testEnvironment": "jsdom",
-    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  // Add the next three options if using TypeScript
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  preset: "ts-jest",
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.[t|j]sx?$": "ts-jest",
+    "^.+\\.svg?$": "<rootDir>/transform.js",
+    "^.+\\.scss?$": "<rootDir>/transform.js"
+  },
+  "testEnvironment": "jsdom",
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$"
 };
